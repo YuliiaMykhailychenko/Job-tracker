39e74637c8a1fababf99a75bc65da2f6a4e1482a
YuliiaMykhailychenko <mykhailychenkoyuliia@gmail.com>
2025-05-23 13:06:35 +0200

    Add watchlist URLs and update root URL

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.application.urls")),
+    path("watchlist/", include("apps.watchlist.urls")),
 ]
